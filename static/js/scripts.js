85102beb24e2064731735546c5d2dfe935ad889d
Moises Oliveira <moisesoliveira@debian>
2016-05-04 09:20:06 -0300

    primeiro commit

---
+++ static/js/scripts.js
@@ -0,0 +1,431 @@
+//Funções das músicas dos Beatles
+function heyjude(){
+	var campo1 = document.getElementById("c1");
+	var campo2 = document.getElementById("c2");
+	var campo3 = document.getElementById("c3");
+	var campo4 = document.getElementById("c4");
+	var campo5 = document.getElementById("c5");
+	var campo6 = document.getElementById("c6");
+	var campo7 = document.getElementById("c7");
+	var campo8 = document.getElementById("c8");
+	var campo9 = document.getElementById("c9");
+	var campo10 = document.getElementById("c10");
+		
+	if(campo1.value.toLowerCase() == "make"){
+		campo1.style.color = "blue";
+	}
+	else{
+		campo1.style.color = "red";
+	}
+
+	if(campo2.value.toLowerCase() == "start"){
+		campo2.style.color = "blue";
+	}
+	else{
+		campo2.style.color = "red";
+	}
+
+	if(campo3.value.toLowerCase() == "were"){
+		campo3.style.color = "blue";
+	}
+	else{
+		campo3.style.color = "red";
+	}
+
+	if(campo4.value.toLowerCase() == "better"){
+		campo4.style.color = "blue";
+	}
+	else{
+		campo4.style.color = "red";
+	}
+
+	if(campo5.value.toLowerCase() == "world"){
+		campo5.style.color = "blue";
+	}
+	else{
+		campo5.style.color = "red";
+	}
+
+	if(campo6.value.toLowerCase() == "little"){
+		campo6.style.color = "blue";
+	}
+	else{
+		campo6.style.color = "red";
+	}
+
+	if(campo7.value.toLowerCase() == "remember"){
+		campo7.style.color = "blue";
+	}
+	else{
+		campo7.style.color = "red";
+	}
+
+	if(campo8.value.toLowerCase() == "someone"){
+		campo8.style.color = "blue";
+	}
+	else{
+		campo8.style.color = "red";
+	}
+
+	if(campo9.value.toLowerCase() == "need"){
+		campo9.style.color = "blue";
+	}
+	else{
+		campo9.style.color = "red";
+	}
+
+	if(campo10.value.toLowerCase() == "sad"){
+		campo10.style.color = "blue";
+	}
+	else{
+		campo10.style.color = "red";
+	}
+
+}
+
+function letitbe(){
+	var campo1 = document.getElementById("c1");
+	var campo2 = document.getElementById("c2");
+	var campo3 = document.getElementById("c3");
+	var campo4 = document.getElementById("c4");
+	var campo5 = document.getElementById("c5");
+	var campo6 = document.getElementById("c6");
+	var campo7 = document.getElementById("c7");
+	var campo8 = document.getElementById("c8");
+	var campo9 = document.getElementById("c9");
+	var campo10 = document.getElementById("c10");
+
+	if(campo1.value.toLowerCase() == "mother"){
+		campo1.style.color = "blue";
+	}
+	else{
+		campo1.style.color = "red";
+	}
+
+	if(campo2.value.toLowerCase() == "right"){
+		campo2.style.color = "blue";
+	}
+	else{
+		campo2.style.color = "red";
+	}
+
+	if(campo3.value.toLowerCase() == "words"){
+		campo3.style.color = "blue";
+	}
+	else{
+		campo3.style.color = "red";
+	}
+
+	if(campo4.value.toLowerCase() == "broken"){
+		campo4.style.color = "blue";
+	}
+	else{
+		campo4.style.color = "red";
+	}
+
+	if(campo5.value.toLowerCase() == "chance"){
+		campo5.style.color = "blue";
+	}
+	else{
+		campo5.style.color = "red";
+	}
+
+	if(campo6.value.toLowerCase() == "whisper"){
+		campo6.style.color = "blue";
+	}
+	else{
+		campo6.style.color = "red";
+	}
+
+	if(campo7.value.toLowerCase() == "tomorrow"){
+		campo7.style.color = "blue";
+	}
+	else{
+		campo7.style.color = "red";
+	}
+
+	if(campo8.value.toLowerCase() == "music"){
+		campo8.style.color = "blue";
+	}
+	else{
+		campo8.style.color = "red";
+	}
+
+	if(campo9.value.toLowerCase() == "will"){
+		campo9.style.color = "blue";
+	}
+	else{
+		campo9.style.color = "red";
+	}
+
+	if(campo10.value.toLowerCase() == "be"){
+		campo10.style.color = "blue";
+	}
+	else{
+		campo10.style.color = "red";
+	}
+}
+
+function yesterday(){
+	var campo1 = document.getElementById("c1");
+	var campo2 = document.getElementById("c2");
+	var campo3 = document.getElementById("c3");
+	var campo4 = document.getElementById("c4");
+	var campo5 = document.getElementById("c5");
+	var campo6 = document.getElementById("c6");
+	var campo7 = document.getElementById("c7");
+	
+
+	if(campo1.value.toLowerCase() == "believe"){
+		campo1.style.color = "blue";
+	}
+	else{
+		campo1.style.color = "red";
+	}
+
+	if(campo2.value.toLowerCase() == "shadow"){
+		campo2.style.color = "blue";
+	}
+	else{
+		campo2.style.color = "red";
+	}
+
+	if(campo3.value.toLowerCase() == "know"){
+		campo3.style.color = "blue";
+	}
+	else{
+		campo3.style.color = "red";
+	}
+
+	if(campo4.value.toLowerCase() == "something"){
+		campo4.style.color = "blue";
+	}
+	else{
+		campo4.style.color = "red";
+	}
+
+	if(campo5.value.toLowerCase() == "easy"){
+		campo5.style.color = "blue";
+	}
+	else{
+		campo5.style.color = "red";
+	}
+
+	if(campo6.value.toLowerCase() == "yesterday"){
+		campo6.style.color = "blue";
+	}
+	else{
+		campo6.style.color = "red";
+	}
+
+	if(campo7.value.toLowerCase() == "need"){
+		campo7.style.color = "blue";
+	}
+	else{
+		campo7.style.color = "red";
+	}
+}
+
+function blackbird(){
+	var campo1 = document.getElementById("c1");
+	var campo2 = document.getElementById("c2");
+	var campo3 = document.getElementById("c3");
+	var campo4 = document.getElementById("c4");
+	var campo5 = document.getElementById("c5");
+	var campo6 = document.getElementById("c6");
+	var campo7 = document.getElementById("c7");
+	
+
+	if(campo1.value.toLowerCase() == "learn"){
+		campo1.style.color = "blue";
+	}
+	else{
+		campo1.style.color = "red";
+	}
+
+	if(campo2.value.toLowerCase() == "waiting"){
+		campo2.style.color = "blue";
+	}
+	else{
+		campo2.style.color = "red";
+	}
+
+	if(campo3.value.toLowerCase() == "singing"){
+		campo3.style.color = "blue";
+	}
+	else{
+		campo3.style.color = "red";
+	}
+
+	if(campo4.value.toLowerCase() == "were"){
+		campo4.style.color = "blue";
+	}
+	else{
+		campo4.style.color = "red";
+	}
+
+	if(campo5.value.toLowerCase() == "black"){
+		campo5.style.color = "blue";
+	}
+	else{
+		campo5.style.color = "red";
+	}
+	if(campo6.value.toLowerCase() == "blackbird"){
+		campo6.style.color = "blue";
+	}
+	else{
+		campo6.style.color = "red";
+	}
+
+	if(campo7.value.toLowerCase() == "moment"){
+		campo7.style.color = "blue";
+	}
+	else{
+		campo7.style.color = "red";
+	}
+}
+
+function twistandshout(){
+	var campo1 = document.getElementById("c1");
+	var campo2 = document.getElementById("c2");
+	var campo3 = document.getElementById("c3");
+	var campo4 = document.getElementById("c4");
+	var campo5 = document.getElementById("c5");
+	var campo6 = document.getElementById("c6");
+	var campo7 = document.getElementById("c7");
+	
+
+	if(campo1.value.toLowerCase() == "baby"){
+		campo1.style.color = "blue";
+	}
+	else{
+		campo1.style.color = "red";
+	}
+
+	if(campo2.value.toLowerCase() == "good"){
+		campo2.style.color = "blue";
+	}
+	else{
+		campo2.style.color = "red";
+	}
+
+	if(campo3.value.toLowerCase() == "work"){
+		campo3.style.color = "blue";
+	}
+	else{
+		campo3.style.color = "red";
+	}
+
+	if(campo4.value.toLowerCase() == "little"){
+		campo4.style.color = "blue";
+	}
+	else{
+		campo4.style.color = "red";
+	}
+
+	if(campo5.value.toLowerCase() == "shout"){
+		campo5.style.color = "blue";
+	}
+	else{
+		campo5.style.color = "red";
+	}
+	if(campo6.value.toLowerCase() == "closer"){
+		campo6.style.color = "blue";
+	}
+	else{
+		campo6.style.color = "red";
+	}
+
+	if(campo7.value.toLowerCase() == "mine"){
+		campo7.style.color = "blue";
+	}
+	else{
+		campo7.style.color = "red";
+	}
+}
+//Fim das Funções
+
+//Funções das músicas dos Scorpions
+function stilllovingyou(){
+	var campo1 = document.getElementById("c1");
+	var campo2 = document.getElementById("c2");
+	var campo3 = document.getElementById("c3");
+	var campo4 = document.getElementById("c4");
+	var campo5 = document.getElementById("c5");
+	var campo6 = document.getElementById("c6");
+	var campo7 = document.getElementById("c7");
+	var campo8 = document.getElementById("c8");
+	var campo9 = document.getElementById("c9");
+	var campo10 = document.getElementById("c10");
+		
+	if(campo1.value.toLowerCase() == ""){
+		campo1.style.color = "blue";
+	}
+	else{
+		campo1.style.color = "red";
+	}
+
+	if(campo2.value.toLowerCase() == ""){
+		campo2.style.color = "blue";
+	}
+	else{
+		campo2.style.color = "red";
+	}
+
+	if(campo3.value.toLowerCase() == ""){
+		campo3.style.color = "blue";
+	}
+	else{
+		campo3.style.color = "red";
+	}
+
+	if(campo4.value.toLowerCase() == ""){
+		campo4.style.color = "blue";
+	}
+	else{
+		campo4.style.color = "red";
+	}
+
+	if(campo5.value.toLowerCase() == ""){
+		campo5.style.color = "blue";
+	}
+	else{
+		campo5.style.color = "red";
+	}
+
+	if(campo6.value.toLowerCase() == ""){
+		campo6.style.color = "blue";
+	}
+	else{
+		campo6.style.color = "red";
+	}
+
+	if(campo7.value.toLowerCase() == ""){
+		campo7.style.color = "blue";
+	}
+	else{
+		campo7.style.color = "red";
+	}
+
+	if(campo8.value.toLowerCase() == ""){
+		campo8.style.color = "blue";
+	}
+	else{
+		campo8.style.color = "red";
+	}
+
+	if(campo9.value.toLowerCase() == ""){
+		campo9.style.color = "blue";
+	}
+	else{
+		campo9.style.color = "red";
+	}
+
+	if(campo10.value.toLowerCase() == ""){
+		campo10.style.color = "blue";
+	}
+	else{
+		campo10.style.color = "red";
+	}
+
+}
